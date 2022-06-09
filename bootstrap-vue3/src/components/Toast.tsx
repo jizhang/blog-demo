efdf90767951a50318047949d3c1ed9d094e100e
Ji ZHANG <zhangji87@gmail.com>
2022-06-09 23:05:54 +0800

    Use functional component

--- bootstrap-vue3/src/components/Toast.tsx
+++ bootstrap-vue3/src/components/Toast.tsx
@@ -1,36 +1,27 @@
-import { createApp, ref, onMounted, defineComponent } from 'vue'
+import { createApp, VNodeRef } from 'vue'
 import { Toast } from 'bootstrap'
 
-const MyToast = defineComponent({
-  props: {
-    message: String,
-  },
+function makeToast(props: { message: string }) {
+  const saveRef: VNodeRef = (el) => {
+    new Toast(el as HTMLElement).show()
+  }
 
-  setup(props) {
-    const toastRef = ref<HTMLElement | null>(null)
-    onMounted(() => {
-      if (toastRef.value) {
-        new Toast(toastRef.value).show()
-      }
-    })
-
-    return () => (
-      <div class="toast position-fixed top-0 start-50 translate-middle-x mt-3" ref={toastRef}>
-        <div class="d-flex">
-          <div class="toast-body">
-            {props.message}
-          </div>
-          <button type="button" class="btn-close m-auto me-3" data-bs-dismiss="toast"></button>
+  return (
+    <div class="toast position-fixed top-0 start-50 translate-middle-x mt-3" ref={saveRef}>
+      <div class="d-flex">
+        <div class="toast-body">
+          {props.message}
         </div>
+        <button type="button" class="btn-close m-auto me-3" data-bs-dismiss="toast"></button>
       </div>
-    )
-  },
-})
+    </div>
+  )
+}
 
 export default (message: string) => {
   const div = document.createElement('div')
   document.body.appendChild(div)
-  createApp(MyToast, { message }).mount(div)
+  createApp(makeToast, { message }).mount(div)
 }
 
 // TODO destroy
