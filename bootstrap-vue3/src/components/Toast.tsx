70e5401f34341ea327bb01ab9a0476db3fc77b7a
Ji ZHANG <zhangji87@gmail.com>
2022-06-09 22:42:57 +0800

    Show toast with component

--- bootstrap-vue3/src/components/Toast.tsx
+++ bootstrap-vue3/src/components/Toast.tsx
@@ -1,13 +1,36 @@
-import { createApp } from 'vue'
+import { createApp, ref, onMounted, defineComponent } from 'vue'
+import { Toast } from 'bootstrap'
 
-const makeToast = (props: any) => {
-  return <b>{ props.message }</b>
-}
+const MyToast = defineComponent({
+  props: {
+    message: String,
+  },
+
+  setup(props) {
+    const toastRef = ref<HTMLElement | null>(null)
+    onMounted(() => {
+      if (toastRef.value) {
+        new Toast(toastRef.value).show()
+      }
+    })
+
+    return () => (
+      <div class="toast position-fixed top-0 start-50 translate-middle-x mt-3" ref={toastRef}>
+        <div class="d-flex">
+          <div class="toast-body">
+            {props.message}
+          </div>
+          <button type="button" class="btn-close m-auto me-3" data-bs-dismiss="toast"></button>
+        </div>
+      </div>
+    )
+  },
+})
 
 export default (message: string) => {
   const div = document.createElement('div')
-  document.querySelector('#app')?.appendChild(div)
-  createApp(makeToast, { message }).mount(div)
+  document.body.appendChild(div)
+  createApp(MyToast, { message }).mount(div)
 }
 
 // TODO destroy
--- bootstrap-vue3/src/views/Demo3.vue
+++ bootstrap-vue3/src/views/Demo3.vue
@@ -1,16 +1,7 @@
 <script setup lang="ts">
-import { onMounted, ref } from 'vue'
-import { Toast } from 'bootstrap'
+import { ref } from 'vue'
 import Modal from '../components/Modal.vue'
 import MyToast from '../components/Toast'
-
-const toastRef = ref<HTMLElement | null>(null)
-let toast: Toast
-onMounted(() => {
-  if (toastRef.value) {
-    toast = new Toast(toastRef.value)
-  }
-})
 
 const dialogVisible = ref(false)
 
@@ -24,10 +15,8 @@ function closeModal() {
 
 function saveChanges() {
   closeModal()
-  toast.show()
+  MyToast('Changed saved.')
 }
-
-MyToast('hello world1')
 </script>
 
 <template>
@@ -42,13 +31,4 @@ MyToast('hello world1')
       <button type="button" class="btn btn-primary" @click="saveChanges">Save changes</button>
     </template>
   </Modal>
-
-  <div class="toast position-fixed top-0 start-50 translate-middle-x mt-3" ref="toastRef">
-    <div class="d-flex">
-      <div class="toast-body">
-        Changes saved.
-      </div>
-      <button type="button" class="btn-close m-auto me-3" data-bs-dismiss="toast"></button>
-    </div>
-  </div>
 </template>
