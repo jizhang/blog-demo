7e63bf947ce74825ee848fb0302354390075daaa
Ji ZHANG <zhangji87@gmail.com>
2022-06-16 10:37:40 +0800

    Add openapi spec to get_post_list (#13)
    * Add openapi spec to get_post_list
    
    * Add page & sort

--- openapi-server/oasis/views.py
+++ openapi-server/oasis/views.py
@@ -1,15 +1,54 @@
-from flask import jsonify, Response
+from flask import request, jsonify, Response
 
 from oasis import app, db
 from oasis.models import Post
-from oasis.schamas import post_schema
+from oasis.schemas import post_schema
+
+PAGE_SIZE = 10
 
 
 @app.get('/api/post/list')
 def get_post_list() -> Response:
-    posts = db.session.query(Post).\
-        order_by(Post.updated_at.desc()).\
-        all()
+    """
+    ---
+    get:
+      summary: Get post list.
+      tags: [post]
+      x-swagger-router-controller: oasis.views
+      operationId: get_post_list
+      parameters:
+        - in: query
+          name: page
+          schema:
+            type: integer
+            minimum: 1
+        - in: query
+          name: sort
+          schema:
+            type: string
+            enum: [asc, desc]
+      responses:
+        '200':
+          description: OK
+          content:
+            application/json:
+              schema:
+                type: object
+                properties:
+                  posts:
+                    type: array
+                    items:
+                      $ref: '#/components/schemas/Post'
+    """
+    page = int(request.args.get('page', '1'))
+    sort = request.args.get('sort', 'desc')
+
+    query = db.session.query(Post)
+    if sort == 'desc':
+        query = query.order_by(Post.updated_at.desc())
+
+    query = query.offset((page - 1) * PAGE_SIZE).limit(PAGE_SIZE)
+    posts = query.all()
     return jsonify(posts=post_schema.dump(posts, many=True))
 
 
