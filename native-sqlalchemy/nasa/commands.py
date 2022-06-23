6c6f6cdec2de9d5a27f91640715a7653609e7659
Ji ZHANG <zhangji87@gmail.com>
2022-06-23 14:32:43 +0800

    SQLAlchemy multiple binds (#17)
    * Add sqlalchemy_multi
    
    * Use product db
    
    * Fix typing

--- native-sqlalchemy/nasa/commands.py
+++ native-sqlalchemy/nasa/commands.py
@@ -1,15 +1,32 @@
-from nasa import app, db
+from nasa import app, db, db_multi
 from nasa.models import Base, User
+
+from sqlalchemy import text
 
 
 @app.cli.command()
-def init_db():
+def init_db() -> None:
     """Initialize database."""
     Base.metadata.create_all(db.session.get_bind())
 
 
 @app.cli.command()
-def user_count():
+def user_count() -> None:
     """Get user count."""
     result = db.session.query(User).count()
     app.logger.info(f'User count: {result}')
+
+
+@app.cli.command()
+def product_db() -> None:
+    """Access product database."""
+    db_file = db_multi.get_session('product_db').execute(
+        text("""
+        SELECT `file` FROM pragma_database_list
+        WHERE `name` = :name
+        """),
+        {
+            'name': 'main',
+        }
+    ).scalar()
+    app.logger.info(f'Database file: {db_file}')
