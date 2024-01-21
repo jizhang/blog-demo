edbc597db8d998b8cd5cd6cb64de92a79362d3f5
Jerry <zhangji87@gmail.com>
2024-01-21 15:46:28 +0800

    Serialization

--- api-pydantic/apipydantic/views/user.py
+++ api-pydantic/apipydantic/views/user.py
@@ -1,18 +1,50 @@
+from dataclasses import dataclass
 from datetime import datetime
+from typing import Annotated
 
 from flask import abort, request
-from pydantic import BaseModel, ConfigDict
+from pydantic import (
+    BaseModel,
+    ConfigDict,
+    Field,
+    PlainSerializer,
+    computed_field,
+)
 
 from apipydantic import app
 from apipydantic.services import user as user_svc
 
 
+def format_datetime(dt: datetime):
+    return dt.strftime('%Y-%m-%d %H:%M:%S')
+
+
+CustomDatetime = Annotated[datetime, PlainSerializer(format_datetime)]
+
+
 class User(BaseModel):
     model_config = ConfigDict(from_attributes=True)
 
-    id: int
+    id: int = Field(serialization_alias='user_id')
     username: str
-    last_login: datetime
+    last_login: CustomDatetime
+
+    @computed_field  # type: ignore[misc]
+    @property
+    def last_login_timestamp(self) -> float:
+        return self.last_login.timestamp()
+
+    # @field_serializer('last_login')
+    # def serialize_last_login(self, value: datetime):
+    #     return value.strftime('%Y-%m-%d %H:%M:%S')
+
+    # @model_serializer
+    # def serialize_model(self) -> dict[str, Any]:
+    #     return {
+    #         'id': self.id,
+    #         'username': self.username,
+    #         'last_login': self.last_login.strftime('%Y-%m-%d %H:%M:%S'),
+    #     }
 
 
 class UserListResponse(BaseModel):
@@ -20,10 +52,19 @@ class UserListResponse(BaseModel):
     total_count: int
 
 
+@dataclass
+class UserDTO:
+    id: int
+    username: str
+    last_login: datetime
+
+
 @app.get('/current-user')
 def current_user() -> dict:
-    user = User(id=1, username='jizhang', last_login=datetime.now())
-    return user.model_dump(mode='json')
+    # user = User(id=1, username='jizhang', last_login=datetime.now())
+    user = User.model_validate({'id': 1, 'username': 'jizhang', 'last_login': datetime.now()})
+    # user = User.model_validate(UserDTO(1, 'jizhang', datetime.now()))
+    return user.model_dump(mode='json', by_alias=True)
 
 
 @app.get('/get-user')
